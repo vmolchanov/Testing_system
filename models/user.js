f695a46eeb1504c83fcf759221c6cb5ec2ea0f53
vmolchanov <Molchanov96@yandex.ru>
2017-09-03 18:31:07 +0300

    Add jsdoc for model of user

--- models/user.js
+++ models/user.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const ObjectId = require("mongodb").ObjectId;
 
-
 mongoose.connect(config.mongoose.url);
 
 let UserSchema = mongoose.Schema({
@@ -25,6 +24,12 @@ let UserSchema = mongoose.Schema({
 
 let User = module.exports = mongoose.model("User", UserSchema);
 
+
+/**
+ * Добавление в базу данных нового пользователя
+ * @param newUser {Object} - данные о новом пользователе
+ * @param callback {Function} - функция обратного вызова
+ */
 module.exports.createUser = (newUser, callback) => {
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -34,17 +39,34 @@ module.exports.createUser = (newUser, callback) => {
     });
 };
 
+
+/**
+ * Получить пользователя из базы данных по email
+ * @param email {string} - email, по которому ведется поиск
+ * @param callback {Function} - функция обратного вызова
+ */
 module.exports.getUserByEmail = (email, callback) => {
     let query = { email: email };
     User.findOne(query, callback);
 };
 
 
+/**
+ * Получить пользователя из базы данных по id
+ * @param id {String} - идентификатор, по которому ведется поиск
+ * @param callback {Function} - функция обратного вызова
+ */
 module.exports.getUserById = (id, callback) => {
     User.findOne(ObjectId(id), callback);
 };
 
 
+/**
+ * Сравнивает переданный пароль с паролем из базы данных
+ * @param candidatePassword {string} - сравниваемый пароль
+ * @param hash {string} - хэш пароля из базы данных
+ * @param callback {Function} - функция обратного вызова
+ */
 module.exports.comparePassword = (candidatePassword, hash, callback) => {
     bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
         if (err) {
@@ -55,6 +77,11 @@ module.exports.comparePassword = (candidatePassword, hash, callback) => {
 };
 
 
+/**
+ * Устанавливает пользователю новый пароль в базе данных
+ * @param user {Object} - пользователь, пароль которого будет обновлен
+ * @param callback {Function} - функция обратного вызова
+ */
 module.exports.setNewPassword = (user, callback) => {
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(user.password, salt, (err, hash) => {
@@ -62,4 +89,13 @@ module.exports.setNewPassword = (user, callback) => {
             User.update(query, { password: hash }, callback);
         });
     });
+};
+
+
+/**
+ * Получает всех пользователей из базы данных
+ * @param callback {Function} - функция обратного вызова
+ */
+module.exports.getUsers = callback => {
+    User.find(callback);
 };
