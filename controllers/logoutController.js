3a2f06bfd2eb53a620ff782a235498476754ff08
vmolchanov <Molchanov96@yandex.ru>
2017-09-16 15:07:07 +0300

    Add clear cookie after logout

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,4 +1,9 @@
 module.exports = (req, res) => {
+    res.clearCookie("testId");
+    res.clearCookie("questionIndexes");
+    res.clearCookie("currentIndex");
+    res.clearCookie("correctAnswer");
+
     req.logout();
     res.redirect("/");
 };
